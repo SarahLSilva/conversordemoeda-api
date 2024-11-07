b5f23eee87156c2957a2928eee26bafb054262d5
Sarah <sarah.lorena@alunoescolassociais.org.br>
2024-11-07 07:37:06 -0300

    complementa java

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
+
+
 const apiKey = '321bb29611b0c54a60e61403';
-const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}321bb29611b0c54a60e61403/latest/`;
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
 
 // Função para buscar taxa  de cambio da API
 
@@ -19,20 +21,39 @@ async function getExchageRate(fromCurrency, toCurrency){
     }
 }
 
-document.getElementById('conteiner').addEventListener('submit',async function(event){
+    document.getElementById('currencyForm').addEventListener('submit', async function(event){
     event.preventDefault();
 
-    const valor = parseFloat(document.getElementById('amaount').value);
-    const fromCurrency = document.getElementById('formCurrency').value;
+    const valor = parseFloat(document.getElementById('Tamanho').value);
+    const fromCurrency = document.getElementById('fromCurrency').value;
     const toCurrency = document.getElementById('toCurrency').value;
 
     const exchangeRate = await getExchageRate(fromCurrency, toCurrency);
 
     if(exchangeRate){
         const convertedValue = valor * exchangeRate;
-        conversao.text =`Resultado ${convertedValue.toFixed(2)} ${toCurrency}`;
+        const conversao = document.getElementById('conversao');
+        conversao.textContent =`Resultado ${convertedValue.toFixed(2)} ${toCurrency}`;
     }else{
-        alert('Erro de buscar cotaçãO. Tente novamente')
+        alert('Erro de buscar cotação. Tente novamente')
     }
 
 });
+
+// Atualiza o símbolo da moeda ao selecionar uma nova moeda
+function updateCurrency() {
+    const fromCurrency = document.getElementById('fromCurrency').value;
+    const currencySymbol = document.getElementById('currencySymbol');
+   
+    // Define o símbolo da moeda de acordo com a seleção
+    if (fromCurrency === "USD") {
+      currencySymbol.textContent = "$";
+    } else if (fromCurrency === "EUR") {
+      currencySymbol.textContent = "€";
+    } else if (fromCurrency === "BRL") {
+      currencySymbol.textContent = "R$";
+    }
+
+    // Atualiza o placeholder
+    document.getElementById('Tamanho').placeholder = "Valor";
+  }
